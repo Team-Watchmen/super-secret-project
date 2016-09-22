1e85dd4f8689dde790a837e503ec220a81b216af
kalostoykov <kalo.stoykov@gmail.com>
2016-09-22 17:19:09 +0300

    Initial commit

---
+++ scripts/templates.js
@@ -0,0 +1,18 @@
+var templates = (function(){
+
+    function get(name) {
+        var promise = new Promise(function(resolve, reject) {
+            var url = `templates/${name}.handlebars`;
+            $.get(url, function(templateHtml) {
+                var template = Handlebars.compile(templateHtml);
+                resolve(template);
+            });
+        });
+
+        return promise;
+    }
+
+    return {
+        get : get
+    };
+}());
