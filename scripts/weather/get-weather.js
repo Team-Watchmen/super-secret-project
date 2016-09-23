fa966465a58bd986dd582827b323fe8cdc5ff8f3
shakuu <todorov.h@gmail.com>
2016-09-23 23:58:34 +0300

    create getWeather module exporting the weather functions

--- scripts/weather/get-weather.js
+++ scripts/weather/get-weather.js
@@ -1,10 +1,12 @@
-import {getWeatherOneDay} from './get-current-weather-for-location.js';
+const getWeather = (() => {
+    import {getWeatherOneDay} from './get-current-weather-for-location.js';
+    import {getWeatherFiveDay} from './get-five-day-weather-for-location.js';
+    import {getWeatherFourteenDay} from './get-fourteen-day-weather-for-location.js';
 
-const getWeather = (()=> {
-    
-
-    return{
-        oneDay: getWeatherOneDay
+    return {
+        oneDay: getWeatherOneDay,
+        fiveDay: getWeatherFiveDay,
+        fourteenDay: getWeatherFourteenDay
     };
 })();
 
