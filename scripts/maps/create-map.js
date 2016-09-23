962de0c0a66c743d11718188f987ee2e89392783
shakuu <todorov.h@gmail.com>
2016-09-24 00:37:30 +0300

    created createMap Module

---
+++ scripts/maps/create-map.js
@@ -0,0 +1,28 @@
+const createMap = (() => {
+    // To initialize the map, we need to refer to google maps API in index.html.
+    function initializeMap(latitude, longitude, idSelector) {
+        const myCenter = new google.maps.LatLng(latitude, longitude)
+
+        // Create map with center and type
+        const mapProp = {
+            center: myCenter,
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        // Attach it to selector.
+        const map = new google.maps.Map((idSelector), mapProp);
+
+        // Get little red marker in map center.
+        new google.maps.Marker({
+            position: myCenter,
+            map: map
+        });
+    }
+
+    return {
+        init: initializeMap
+    };
+})();
+
+export { createMap };
