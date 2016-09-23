bc91a3a076ea63f21a5d5af30f9033f4c0806e25
Deyan Aleksandrov <deyanaaleksandrov@gmail.com>
2016-09-23 13:31:34 +0300

    Added five day forecast data in similar fashion to current weather data. Please not that data construction is different!!

--- scripts/system-config.js
+++ scripts/system-config.js
@@ -4,6 +4,7 @@ SystemJS.config({
         'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
         'main': './scripts/main.js',
-        'getWeather':'./scripts/getWeatherData.js'
+        'get-current-weather':'./scripts/get-current-weather-for-location.js',
+        'get-five-day-forecast':'./scripts/get-five-day-weather-for-location.js'
     }
 });
