ee0c0d6d02a465a60441e8eb77837b09915316c2
Deyan Aleksandrov <deyanaaleksandrov@gmail.com>
2016-09-23 13:01:26 +0300

    Function promise for current weather data by city name added and imported through the system-config

--- scripts/system-config.js
+++ scripts/system-config.js
@@ -3,6 +3,7 @@ SystemJS.config({
     map: {
         'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
-        'main': './scripts/main.js'
+        'main': './scripts/main.js',
+        'getWeather':'./scripts/getWeatherData.js'
     }
 });
