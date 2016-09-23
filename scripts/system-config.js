fc7fe36ce732a2cabd3ed6048a1a7c540e6edef8
Deyan Aleksandrov <deyanaaleksandrov@gmail.com>
2016-09-23 13:41:20 +0300

    added last weather forecast

--- scripts/system-config.js
+++ scripts/system-config.js
@@ -5,6 +5,7 @@ SystemJS.config({
         'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
         'main': './scripts/main.js',
         'get-current-weather':'./scripts/get-current-weather-for-location.js',
-        'get-five-day-forecast':'./scripts/get-five-day-weather-for-location.js'
+        'get-five-day-forecast':'./scripts/get-five-day-weather-for-location.js',
+        'get-fourteen-day-forecast':'./scripts/get-fourteen-day-weather-for-location.js'
     }
 });
