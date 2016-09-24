8a2caf146c3cd88e6e7d9d12ae8d589eda28ce97
shakuu <todorov.h@gmail.com>
2016-09-25 00:27:13 +0300

    created WeatherApp class

---
+++ scripts/app/weather-app.js
@@ -0,0 +1,12 @@
+import {WeatherProvider} from './weather/get-weather.js';
+import {MapProvider} from './maps/create-map.js';
+
+const WeatherApp = (() => {
+    class WeatherApp {
+        constructor(userManager, weatherProvider, mapsProvider) {
+
+        }
+    }
+})();
+
+export { WeatherApp };
