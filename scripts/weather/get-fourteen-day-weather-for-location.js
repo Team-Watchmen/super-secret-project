f3dc60a71bd8bace2e8e25040a111917766c4660
Deyan Aleksandrov <deyanaaleksandrov@gmail.com>
2016-09-24 20:33:01 +0300

    Update get-fourteen-day-weather-for-location.js
    Fix some wrong names

--- scripts/weather/get-fourteen-day-weather-for-location.js
+++ scripts/weather/get-fourteen-day-weather-for-location.js
@@ -7,8 +7,8 @@ const getWeatherFourteenDay = function ($cityName) {
     // I put a string just for practice, it can work with click event function or input event function, whatever we decide like the other function
     // let $cityName = 'Pernik';
 
-    // 5 day data promise function.
-    const functionThatReturnFiveDayForecastData = (success) => {
+    // 14-day data promise function.
+    const functionThatReturnFourteenDayForecastData = (success) => {
         return new Promise((resolve, reject) => {
             if (success) {
                 resolve(success)
@@ -19,7 +19,7 @@ const getWeatherFourteenDay = function ($cityName) {
         });
     };
 
-    return functionThatReturnFiveDayForecastData(getFiveDayJSONForCity($cityName));
+    return functionThatReturnFourteenDayForecastData(getFourteenDayJSONForCity($cityName));
     // .then(data=> {
     //     // console.log("FOURTEEN-DAY FORECAST for");
     //     // console.log(data.city.name);
@@ -28,7 +28,7 @@ const getWeatherFourteenDay = function ($cityName) {
     // })
     // .catch(error =>console.log(error));
 
-    function getFiveDayJSONForCity(cityName) {
+    function getFourteenDayJSONForCity(cityName) {
         return $.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q="
             + cityName +
             "&cnt=14&mode=json&units=metric&appid=a28f075ad9633624934634a4d49a37c5");
