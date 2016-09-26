def7a908025dd2a36531e8005766956eb140ffe5
shakuu <todorov.h@gmail.com>
2016-09-26 12:29:14 +0300

    extracted load sdk to file

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
+import { loadFacebookSDK } from './facebook/load-facebook-sdk.js';
 import { WeatherApp } from './app/weather-app.js';
 
-const weatherApp = (() => {   
+const weatherApp = (() => {
     const app = new WeatherApp();
 
     return app;
 })();
 
 $(function () {
+    loadFacebookSDK.load();
     weatherApp.start();
 });
