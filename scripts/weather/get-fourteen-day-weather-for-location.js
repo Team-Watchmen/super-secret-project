7224b0417e37c3389ceda73a88765f023bf62e5c
shakuu <todorov.h@gmail.com>
2016-09-26 13:13:32 +0300

    removed commented code

--- scripts/weather/get-fourteen-day-weather-for-location.js
+++ scripts/weather/get-fourteen-day-weather-for-location.js
@@ -3,10 +3,6 @@
  */
 
 const getWeatherFourteenDay = function ($cityName) {
-
-    // I put a string just for practice, it can work with click event function or input event function, whatever we decide like the other function
-    // let $cityName = 'Pernik';
-
     // 14-day data promise function.
     const functionThatReturnFourteenDayForecastData = (success) => {
         return new Promise((resolve, reject) => {
@@ -20,17 +16,11 @@ const getWeatherFourteenDay = function ($cityName) {
     };
 
     return functionThatReturnFourteenDayForecastData(getFourteenDayJSONForCity($cityName));
-    // .then(data=> {
-    //     // console.log("FOURTEEN-DAY FORECAST for");
-    //     // console.log(data.city.name);
-    //     console.log(data);
-
-    // })
-    // .catch(error =>console.log(error));
 
     function getFourteenDayJSONForCity(cityName) {
-        return $.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q="
-            + cityName +
+        return $.getJSON(
+            "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +
+            cityName +
             "&cnt=14&mode=json&units=metric&appid=a28f075ad9633624934634a4d49a37c5");
     }
 };
